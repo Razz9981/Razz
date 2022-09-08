4c20b3908c382c94d8f30929359c5bcbffadc390
Razz9981 <Rajkharerdk@gmail.com>
2022-09-08 12:55:20 +0100

    initial commit

---
+++ test.java
@@ -0,0 +1,15 @@
+class Demo {
+
+    public void calculate(int a, int b) {
+
+        int c = a * b;
+        System.out.println(c);
+    }
+
+    public static void main(String[] args) {
+        Demo c = new Demo();
+        c.calculate(12, 22);
+
+    }
+
+}
