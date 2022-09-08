6f414c193696d346ad9402d08dd4e090e22f231d
Razz9981 <Rajkharerdk@gmail.com>
2022-09-08 13:07:27 +0100

    edited

--- test.java
+++ test.java
@@ -1,9 +1,9 @@
 class Demo {
 
     public void calculate(int a, int b) {
-
+        int d = a / b;
         int c = a * b;
-        System.out.println(c);
+        System.out.println(c + d);
     }
 
     public static void main(String[] args) {
